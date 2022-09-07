80e1a7cba47340ee79d4626ac629b7cdbc405f72
emsknock <25252659+emsknock@users.noreply.github.com>
2022-09-07 17:22:22 +0300

    Run prettier

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,11 @@
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
