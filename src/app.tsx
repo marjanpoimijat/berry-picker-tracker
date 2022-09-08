f8b71602074e5d3214e4c4c51538a7f22470b020
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-09-08 19:44:24 +0300

    add location

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,37 @@
 import { Text, View, StyleSheet, Dimensions } from "react-native";
 import { registerRootComponent } from "expo";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Berry picker tracker</Text>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} showsUserLocation={true} />
     </View>
   );
 }
@@ -21,6 +46,7 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+    marginTop: 50,
   },
 });
 
