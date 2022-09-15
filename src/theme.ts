93e59a4a95e8b85b7c3af5c8c2cfa7dbc42c914a
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-09-15 15:48:36 +0300

    create theme

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+	colors: {
+		textPrimary: "black",
+		textSecondary: "white",
+		buttonBackgroundColor: "white",
+		primaryBackgroundColor: "grey",
+	},
+	fontSizes: {
+		body: 14,
+		header: 18,
+	},
+	fonts: {
+		main: "System",
+	},
+	fontWeights: {
+		normal: "400",
+		bold: "700",
+	},
+};
+
+export default theme;
