9b5a32de787faf374bd55055ec5042ff76e5051e
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-05 21:28:16 +0300

    create identify user hook

---
+++ src/hooks/use-identify-user.ts
@@ -0,0 +1,8 @@
+//import useUserIdStorage from "./use-user-id-storage";
+
+const useIdentifyUser = () => {
+	//const userIdStorage = useUserIdStorage();
+	// Todo...
+};
+
+export default useIdentifyUser;
