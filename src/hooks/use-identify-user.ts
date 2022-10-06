3c68da5478af7626af23d8409173f3518166d295
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-06 22:06:32 +0300

    create hook to indentify user

--- src/hooks/use-identify-user.ts
+++ src/hooks/use-identify-user.ts
@@ -1,8 +1,22 @@
-//import useUserIdStorage from "./use-user-id-storage";
+import useUserIdStorage from "./use-user-id-storage";
+import { createNewUser } from "../requests";
 
 const useIdentifyUser = () => {
-	//const userIdStorage = useUserIdStorage();
-	// Todo...
+	const userIdStorage = useUserIdStorage();
+
+	const identifyUser = async () => {
+		const idFromStorage = await userIdStorage.getId();
+
+		if (idFromStorage !== null) {
+			console.log(`id found from storage ${idFromStorage}`);
+		} else {
+			console.log(`id not found from storage, creating new user`);
+			const data = await createNewUser();
+			await userIdStorage.setId(data.id);
+		}
+	};
+
+	return identifyUser;
 };
 
 export default useIdentifyUser;
