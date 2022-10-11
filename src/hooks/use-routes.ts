067abe2add28641da9a17e8783d15ef1f22d59eb
Santeri Salonen <72451015+Sanexi@users.noreply.github.com>
2022-10-11 09:28:39 +0300

    Update use-routes.ts

--- src/hooks/use-routes.ts
+++ src/hooks/use-routes.ts
@@ -29,16 +29,14 @@ const useRoutes = () => {
 	 * Function to add now waypoints to current route. Makes http request
 	 * to add location and mnc-code to given routeId.
 	 * TODO: Add timestamps to http request.
-	 * @returns request statuscode
+	 * @returns request response
 	 */
 	const sendWaypoint = async (
 		routeId: string,
 		location: LocationObject,
 		mnc: string
 	) => {
-		// const ts = new Date()
 		const data = await sendNewWaypoint(routeId, location, mnc);
-		console.log("New waypoint sent to server, status:", data);
 		return data;
 	};
 
