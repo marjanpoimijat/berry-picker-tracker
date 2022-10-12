6411736d375cb842fc6ead0517913052cc655c2d
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2022-10-12 11:33:03 +0300

    Add toy screen

---
+++ src/screens/coffee-screen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CoffeeScreen = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Coffee</Text>
+		</View>
+	);
+};
+
+export default CoffeeScreen;
