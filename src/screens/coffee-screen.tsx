8a8a8ec4378df9b765ca7aadd0d223a8203ffd29
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2022-10-12 13:47:16 +0300

    Add content

--- src/screens/coffee-screen.tsx
+++ src/screens/coffee-screen.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const CoffeeScreen = () => {
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>Coffee</Text>
+			<Icon name="coffee" size={60} color="lightblue" />
+			<Icon name="hamburger" size={60} color="brown" />
 		</View>
 	);
 };
