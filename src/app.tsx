9bae36456c30166a65d3102357140260e1a6429a
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-25 23:13:56 +0300

    remove id-storage and replace with redux persist store

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,26 @@
 import { registerRootComponent } from "expo";
 import { NativeRouter } from "react-router-native";
+import { Provider } from "react-redux";
 
 import Main from "./components/main";
-import IdStorage from "./utils/id-storage";
 import WaypointStorage from "./utils/waypoint-storage";
-import UserIdStorageContext from "./contexts/user-id-context";
-import RouteIdStorageContext from "./contexts/route-id-context";
 import WaypointStorageContext from "./contexts/waypoint-context";
 
-const userIdStorage = new IdStorage("user-id");
-const routeIdStorage = new IdStorage("route-id");
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const waypointStorage = new WaypointStorage("waypoint");
 
 const App = () => {
 	return (
 		<NativeRouter>
-			<UserIdStorageContext.Provider value={userIdStorage}>
-				<RouteIdStorageContext.Provider value={routeIdStorage}>
-					<WaypointStorageContext.Provider value={waypointStorage}>
+			<WaypointStorageContext.Provider value={waypointStorage}>
+				<Provider store={store}>
+					<PersistGate loading={null} persistor={persistor}>
 						<Main />
-					</WaypointStorageContext.Provider>
-				</RouteIdStorageContext.Provider>
-			</UserIdStorageContext.Provider>
+					</PersistGate>
+				</Provider>
+			</WaypointStorageContext.Provider>
 		</NativeRouter>
 	);
 };
