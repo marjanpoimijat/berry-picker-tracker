75c1ff0d99976d2728272b3c3b25eace82cfebd5
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-26 23:30:58 +0300

    add waypoint reducer to root

--- src/reducers/root-reducer.ts
+++ src/reducers/root-reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user-reducer";
 import routeReducer from "./route-reducer";
+import waypointReducer from "./waypoint-reducer";
 
 export default combineReducers({
 	user: userReducer,
 	route: routeReducer,
+	waypoints: waypointReducer,
 });
