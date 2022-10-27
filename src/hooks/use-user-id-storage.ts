74425371910a9d0994d061b00dfa8d8664733178
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-27 20:37:21 +0300

    delete previous async storage system

--- src/hooks/use-user-id-storage.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import UserIdStorageContext from "../contexts/user-id-context";
-
-const useUserIdStorage = () => {
-	return useContext(UserIdStorageContext);
-};
-
-export default useUserIdStorage;
--- src/hooks/use-waypoint-storage.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import WaypointStorageContext from "../contexts/waypoint-context";
-
-const useWaypointStorage = () => {
-	return useContext(WaypointStorageContext);
-};
-
-export default useWaypointStorage;
