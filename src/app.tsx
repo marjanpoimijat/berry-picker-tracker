fcc0b260c64432ee6903c361ae8733eca8d9e24f
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-10-27 20:37:01 +0300

    remove unused context providers

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,19 @@
 import { registerRootComponent } from "expo";
 import { NativeRouter } from "react-router-native";
 import { Provider } from "react-redux";
-
-import Main from "./main";
-import WaypointStorage from "./utils/waypoint-storage";
-import WaypointStorageContext from "./contexts/waypoint-context";
-
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
-const waypointStorage = new WaypointStorage("waypoint");
+import Main from "./main";
 
 const App = () => {
 	return (
 		<NativeRouter>
-			<WaypointStorageContext.Provider value={waypointStorage}>
-				<Provider store={store}>
-					<PersistGate loading={null} persistor={persistor}>
-						<Main />
-					</PersistGate>
-				</Provider>
-			</WaypointStorageContext.Provider>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<Main />
+				</PersistGate>
+			</Provider>
 		</NativeRouter>
 	);
 };
