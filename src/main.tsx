492ed5c73f1ed481d228f00a02887c822ff72a7e
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-11-03 22:01:14 +0200

    replace interval tracking with background location tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,28 @@ import { View, StyleSheet } from "react-native";
 import { Navigate, Route, Routes } from "react-router-native";
 import { useEffect } from "react";
 
-import { useTypedDispatch, useTypedSelector } from "./store";
+import { useTypedDispatch } from "./store";
 import { identifyUser } from "./reducers/user-reducer";
-import {
-	storeWaypoint,
-	sendPendingWaypoints,
-} from "./reducers/waypoint-reducer";
 import MapScreen from "./screens/map-screen";
 import SettingsScreen from "./screens/settings-screen";
 import CoffeeScreen from "./screens/coffee-screen";
 import NavigatorTab from "./components/navigator-tab";
-import useInterval from "./hooks/use-interval";
+
+import {
+	defineBackgroundLocationTask,
+	requestPermissions,
+} from "./utils/location-tracking";
 
 const Main = () => {
-	const user = useTypedSelector((state) => state.user);
-	const routeInfo = useTypedSelector((state) => state.route);
-	const waypoints = useTypedSelector((state) => state.waypoints);
 	const dispatch = useTypedDispatch();
 
 	useEffect(() => {
-		dispatch(identifyUser(user.userId));
+		(async () => {
+			defineBackgroundLocationTask(dispatch);
+			await requestPermissions();
+			await dispatch(identifyUser());
+		})();
 	}, [dispatch]);
-
-	useInterval(() => {
-		if (routeInfo.active) {
-			dispatch(storeWaypoint(routeInfo.routeId));
-		}
-	}, user.trackingInterval);
-
-	useInterval(() => {
-		if (routeInfo.active) {
-			dispatch(sendPendingWaypoints(waypoints.pendingWaypoints));
-		}
-	}, user.sendingInterval);
 
 	return (
 		<View style={styles.container}>
