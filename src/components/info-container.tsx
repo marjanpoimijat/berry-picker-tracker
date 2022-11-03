5652575fde2d2dae80928dd790b498a2d86101ec
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-11-03 21:18:56 +0200

    re-activate displaying of coordinates and mnc

--- src/components/info-container.tsx
+++ src/components/info-container.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { LocationObject } from "expo-location";
+import * as Location from "expo-location";
+import * as Cellular from "expo-cellular";
 
 import { useTypedSelector } from "../store";
 import theme from "../theme";
@@ -16,7 +18,7 @@ const styles = StyleSheet.create({
 		borderRadius: 20,
 		padding: 15,
 		textAlign: "center",
-		height: 175,
+		height: 150,
 		shadowColor: "black",
 		shadowOffset: { width: 3, height: 3 },
 		shadowOpacity: 0.8,
@@ -31,35 +33,37 @@ const styles = StyleSheet.create({
  * purposes. Some of the info should be deleted later, but perhaps keep the coordinates?
  */
 const InfoContainer = (): JSX.Element => {
-	const [curLocation] = useState<LocationObject | null>(null);
-	const [mobileNetCode] = useState<string | null>(null);
+	const [curLocation, setCurLocation] = useState<LocationObject | null>(null);
+	const [mobileNetCode, setMobileNetCode] = useState<string | null>(null);
 	const waypoints = useTypedSelector((state) => state.waypoints);
-	const routeId = useTypedSelector((state) => state.route.routeId);
+
+	useEffect(() => {
+		(async () => {
+			const location = await Location.getCurrentPositionAsync({});
+			setCurLocation(location);
+			const networkCode = await Cellular.getMobileNetworkCodeAsync();
+			setMobileNetCode(networkCode);
+		})();
+	}, []);
 
 	return (
 		<View style={styles.infoContainer}>
 			<Text style={{ fontWeight: "bold" }}>Current location:</Text>
 			<Text>
-				-Latitude:{" "}
-				{curLocation === null ? "disabled" : curLocation.coords.latitude}
+				-Latitude: {curLocation === null ? "NA" : curLocation.coords.latitude}
 			</Text>
 			<Text>
-				-Longitude:{" "}
-				{curLocation === null ? "disabled" : curLocation.coords.longitude}
+				-Longitude: {curLocation === null ? "NA" : curLocation.coords.longitude}
 			</Text>
 			<Text style={{ fontWeight: "bold" }}>Cellular network:</Text>
 			<Text>
-				-NMC code: {mobileNetCode === null ? "disabled" : mobileNetCode}
+				-NMC code: {mobileNetCode === null ? "No network" : mobileNetCode}
 			</Text>
 			<Text style={{ fontWeight: "bold" }}>
 				Local waypoints: {waypoints.localWaypoints.length}
 			</Text>
 			<Text style={{ fontWeight: "bold" }}>
 				Pending waypoints: {waypoints.pendingWaypoints.length}
-			</Text>
-			<Text style={{ fontWeight: "bold" }}>
-				Route ID:{" "}
-				{routeId ? `${routeId.substring(0, 11)}...` : "route not active"}
 			</Text>
 		</View>
 	);
