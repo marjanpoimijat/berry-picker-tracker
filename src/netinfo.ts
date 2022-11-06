d99558c20c2df14aa3b765d73ca28acaeb0c6648
Riku Brandt <brandtriku@gmail.com>
2022-11-06 23:47:06 +0200

    docs

--- src/netinfo.ts
+++ src/netinfo.ts
@@ -2,12 +2,20 @@ import NetInfo, {
 	NetInfoState,
 	NetInfoCellularGeneration,
 } from "@react-native-community/netinfo";
-
+/**
+ * Gets network information state and returns it
+ * @returns NetInfoState`
+ */
 export async function NetworkConnectionInformation(): Promise<NetInfoState> {
 	const state = await NetInfo.fetch();
 	return state;
 }
-
+/**
+ * Parses the NetInfoState and returns the cellular generation
+ * if phone is connected via cellular type.
+ * When using expo defaults to null because it uses wifi!
+ * @returns Cellular generation or null`
+ */
 export function getNetworkCellularGeneration(
 	state: NetInfoState
 ): NetInfoCellularGeneration | null {
