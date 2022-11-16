63ee76b80663b801c5610f468bb4d70e34d7831f
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-11-16 21:28:25 +0200

    update build version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
 	name: "Berry picker tracker",
 	slug: "berry-picker-tracker",
 	entryPoint: "./index.js",
-	version: "1.0.0",
+	version: "1.0.8",
 	orientation: "portrait",
 	icon: "./assets/icon.png",
 	splash: {
@@ -19,6 +19,7 @@ export default {
 	ios: {
 		bundleIdentifier: "com.berry.picker.tracker",
 		supportsTablet: true,
+		buildNumber: "9",
 		infoPlist: {
 			UIBackgroundModes: ["location", "fetch"],
 			NSLocationAlwaysAndWhenInUseUsageDescription: "App requires geolocation",
@@ -26,15 +27,40 @@ export default {
 	},
 	android: {
 		package: "com.berry.picker.tracker",
-		versionCode: 1,
+		versionCode: 9,
 		adaptiveIcon: {
 			foregroundImage: "./assets/adaptive-icon.png",
 			backgroundColor: "#FFFFFF",
 		},
+		config: {
+			googleMaps: {
+				apiKey: process.env.MAPS_API,
+			},
+		},
+	},
+	androidStatusBarColor: "#333C4A",
+	androidStatusBar: {
+		barStyle: "light-content",
+		backgroundColor: "#333C4A",
 	},
 	web: {
 		favicon: "./assets/favicon.png",
 	},
+	plugins: [
+		[
+			"expo-build-properties",
+			{
+				android: {
+					compileSdkVersion: 31,
+					targetSdkVersion: 31,
+					buildToolsVersion: "31.0.0",
+				},
+				ios: {
+					deploymentTarget: "13.0",
+				},
+			},
+		],
+	],
 	extra: {
 		uri: process.env.URI,
 		eas: {
