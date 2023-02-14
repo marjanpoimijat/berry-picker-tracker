71d4976e212a23a980cff579c49e5cf1613bb33d
Riku Rauhala <riku@rauhala.io>
2023-02-14 11:52:43 +0200

    Save current location to Redux store
    Co-authored-by: Catrovitch <ale.engelhardt@outlook.com>

--- src/components/coordinate-container.tsx
+++ src/components/coordinate-container.tsx
@@ -4,18 +4,39 @@ import { LocationObject } from "expo-location";
 import * as Location from "expo-location";
 import { languages } from "../languages";
 import Styles from "../styles";
-import { useTypedSelector } from "../store";
+import { useTypedDispatch, useTypedSelector } from "../store";
+import { setCoordinate } from "../reducers/coordinate-reducer";
 
 /**
  * Coordinate container to show current coordinates at top of the map screen.
  */
 const CoordinateContainer = (): JSX.Element => {
 	const language = useTypedSelector((state) => state.language);
+	const location = useTypedSelector((state) => state.coordinate);
 	const [curLocation, setCurLocation] = useState<LocationObject | null>(null);
+
+	const dispatch = useTypedDispatch();
 
 	useEffect(() => {
 		const interval = setInterval(async () => {
 			const location = await Location.getLastKnownPositionAsync({});
+			const initialState = {
+				coords: {
+					latitude: 60.204662,
+					longitude: 24.962535,
+					altitude: null,
+					accuracy: null,
+					altitudeAccuracy: null,
+					heading: null,
+					speed: null,
+				},
+				timestamp: 0,
+			};
+			if (location === null) {
+				dispatch(setCoordinate(initialState));
+			} else {
+				dispatch(setCoordinate(location));
+			}
 			setCurLocation(location);
 		}, 5000);
 
@@ -25,18 +46,18 @@ const CoordinateContainer = (): JSX.Element => {
 	return (
 		<View style={Styles.coordinateContainer}>
 			<Text style={Styles.coordinateItems}>
-				{curLocation === null
+				{location === null
 					? languages["NA"][language]
-					: curLocation.coords.latitude > 0
-					? `${curLocation.coords.latitude} °N`
-					: `${curLocation.coords.latitude} °S`}
+					: location.coords.latitude > 0
+					? `${location.coords.latitude} °N`
+					: `${location.coords.latitude} °S`}
 			</Text>
 			<Text style={Styles.coordinateItems}>
-				{curLocation === null
+				{location === null
 					? languages["NA"][language]
-					: curLocation.coords.longitude > 0
-					? `${curLocation.coords.longitude} °E`
-					: `${curLocation.coords.longitude} °W`}
+					: location.coords.longitude > 0
+					? `${location.coords.longitude} °E`
+					: `${location.coords.longitude} °W`}
 			</Text>
 		</View>
 	);
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,15 @@
 import { combineReducers } from "redux";
-import user from "./user-reducer";
-import route from "./route-reducer";
-import waypoints from "./waypoint-reducer";
-import language from "./language-reducer";
 
-export default combineReducers({ user, route, waypoints, language });
+import coordinate from "./coordinate-reducer";
+import language from "./language-reducer";
+import route from "./route-reducer";
+import user from "./user-reducer";
+import waypoints from "./waypoint-reducer";
+
+export default combineReducers({
+	coordinate,
+	language,
+	route,
+	user,
+	waypoints,
+});
