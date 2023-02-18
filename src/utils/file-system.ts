68b9eb0ffebba48c1110f5823acbabce90ec0fe5
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-02-18 19:17:41 +0200

    clearing cache doesn't use async/await and .then at the same time

--- src/utils/file-system.ts
+++ src/utils/file-system.ts
@@ -6,22 +6,23 @@ import { tileCacheDirectory } from "../constants";
  * Every tile requested from the server will be saved locally
  * for a set amount of time.
  */
-export const makeTileCacheDirectory = () => {
-	FileSystem.makeDirectoryAsync(tileCacheDirectory)
-		.then(() => {
-			console.log("Created tile cache directory at " + tileCacheDirectory);
-		})
-		.catch(() => {
-			//console.log("Tile directory already exists");
+export const makeTileCacheDirectory = async () => {
+	try {
+		console.log("creating tile cache...");
+		await FileSystem.makeDirectoryAsync(tileCacheDirectory, {
+			intermediates: true,
 		});
+		console.log("Created tile cache directory at " + tileCacheDirectory);
+	} catch (error) {
+		console.log("Error creating tile cache directory:", error);
+	}
 };
 
-export const deleteTileCacheDirectory = () => {
-	FileSystem.deleteAsync(tileCacheDirectory)
-		.then(() => {
-			console.log("Deleted tile cache directory");
-		})
-		.catch((err) => {
-			console.log("Map tile deletion error:", err);
-		});
+export const deleteTileCacheDirectory = async () => {
+	try {
+		await FileSystem.deleteAsync(tileCacheDirectory, { idempotent: false });
+		console.log("Deleted tile cache directory");
+	} catch (err) {
+		console.log("Map tile deletion error:", err);
+	}
 };
