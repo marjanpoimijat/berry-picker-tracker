c1c2f6f37a74ed267cb529b21380c6b757939ef5
Riku Rauhala <riku@rauhala.io>
2023-02-20 12:44:41 +0200

    Fix the negative coordinates for South and West coordinates
    Co-authored-by: Catrovitch <ale.engelhardt@outlook.com>

--- src/utils/coordinates.ts
+++ src/utils/coordinates.ts
@@ -1,7 +1,7 @@
 export const parseLatitude = (latitude: number): string => {
-	return latitude > 0 ? `${latitude} °N` : `${latitude} °S`;
+	return latitude > 0 ? `${latitude} °N` : `${-1 * latitude} °S`;
 };
 
 export const parseLongitude = (longitude: number): string => {
-	return longitude > 0 ? `${longitude} °E` : `${longitude} °W`;
+	return longitude > 0 ? `${longitude} °E` : `${-1 * longitude} °W`;
 };
