8bb5288a620fff8f9a92fa6fc3015efacf163ead
Janne Kärki <janne.karki@helsinki.fi>
2023-03-06 15:44:23 +0200

    Refactored code

--- src/track-user.ts
+++ src/track-user.ts
@@ -6,26 +6,24 @@ import {
 import { getUsersLatestRoute } from "./requests";
 
 export const addSharedUser = async () => {
-	secureStoreDeleteAll(); //tämä on poistettava kun jakotoiminnallisuus on valmis
+	secureStoreDeleteAll(); // remove this when sharing functionality is complete
 	const url = Linking.useURL();
-	if (url) {
-		const { hostname, path, queryParams } = Linking.parse(url); //Parsing url for share functionality
-		console.log(`${hostname} ${path} ${queryParams} `);
-		if (queryParams && queryParams.userId) {
-			const userId = queryParams.userId;
-			const alias = queryParams.alias;
-			if (typeof userId === "string") {
-				const data = await getUsersLatestRoute(userId);
-				if (data) {
-					if (data.routeId === undefined) {
-						console.log(`shared userId: ${alias} ${userId} is not found`);
-						return;
-					} else {
-						console.log(`shared userId: ${alias} ${userId} is found`);
-						await secureStoreAddTracked(userId, `${alias}`);
-					}
-				}
-			}
-		}
+	if (!url) {
+		return;
 	}
+	const { queryParams } = Linking.parse(url);
+	if (!queryParams || !queryParams.userId) {
+		return;
+	}
+	const { userId, alias } = queryParams;
+	if (typeof userId !== "string") {
+		return;
+	}
+	const data = await getUsersLatestRoute(userId);
+	if (!data || data.routeId === undefined) {
+		console.log(`shared userId: ${alias} ${userId} is not found`);
+		return;
+	}
+	console.log(`shared userId: ${alias} ${userId} is found`);
+	await secureStoreAddTracked(userId, `${alias}`);
 };
