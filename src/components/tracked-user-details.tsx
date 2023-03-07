540e7a171d596f0efc72ffbe87928b33739e20c0
Riku Rauhala <riku@rauhala.io>
2023-03-07 11:12:48 +0200

    Refactor trakcer user details into smaller components
    Co-authored-by: Catrovitch <ale.engelhardt@outlook.com>

--- src/components/tracked-user-details.tsx
+++ src/components/tracked-user-details.tsx
@@ -17,37 +17,86 @@ const TrackedUserDetails = ({ id, username }: TrackedUser) => {
 	return (
 		<View style={Styles.trackedUserDetailsContainer}>
 			<View style={Styles.trackedUserDetailsNameContainer}>
-				<View style={Styles.trackedUserDetailsDotContainer}>
-					<View
-						style={{
-							...Styles.trackedUserDetailsDot,
-							backgroundColor: colors[id],
-						}}
-					/>
-				</View>
-				<Text style={Styles.trackedUserDetailsUsername}>{username}</Text>
+				<Dot id={id} />
+				<Username username={username} />
 			</View>
 			<View style={Styles.trackedUserDetailsButtonContainer}>
-				<TouchableOpacity
-					onPress={() => setLocationVisible(!locationVisible)}
-					style={{ marginRight: 20 }}
-				>
-					{locationVisible ? (
-						<Button disabled={!locationVisible} name="eye" />
-					) : (
-						<Button disabled={!locationVisible} name="eye-slash" />
-					)}
-				</TouchableOpacity>
-				<TouchableOpacity
-					disabled={!locationVisible}
-					onPress={() => setRouteVisible(!routeVisible)}
-				>
-					<Button disabled={!locationVisible || !routeVisible} name="route" />
-				</TouchableOpacity>
+				<LocationVisibleButton
+					locationVisible={locationVisible}
+					setLocationVisible={setLocationVisible}
+				/>
+				<RouteVisibleButton
+					locationVisible={locationVisible}
+					routeVisible={routeVisible}
+					setRouteVisible={setRouteVisible}
+				/>
 			</View>
 		</View>
 	);
 };
+
+interface DotProps {
+	id: number;
+}
+
+const Dot = ({ id }: DotProps) => (
+	<View style={Styles.trackedUserDetailsDotContainer}>
+		<View
+			style={{
+				...Styles.trackedUserDetailsDot,
+				backgroundColor: colors[id],
+			}}
+		/>
+	</View>
+);
+
+interface UsernameProps {
+	username: string;
+}
+
+const Username = ({ username }: UsernameProps) => (
+	<Text style={Styles.trackedUserDetailsUsername}>{username}</Text>
+);
+
+interface LocationVisibleButtonProps {
+	locationVisible: boolean;
+	setLocationVisible: (arg0: boolean) => void;
+}
+
+const LocationVisibleButton = ({
+	locationVisible,
+	setLocationVisible,
+}: LocationVisibleButtonProps) => (
+	<TouchableOpacity
+		onPress={() => setLocationVisible(!locationVisible)}
+		style={{ marginRight: 20 }}
+	>
+		{locationVisible ? (
+			<Button disabled={!locationVisible} name="eye" />
+		) : (
+			<Button disabled={!locationVisible} name="eye-slash" />
+		)}
+	</TouchableOpacity>
+);
+
+interface RouteVisibleButtonProps {
+	locationVisible: boolean;
+	routeVisible: boolean;
+	setRouteVisible: (arg0: boolean) => void;
+}
+
+const RouteVisibleButton = ({
+	locationVisible,
+	routeVisible,
+	setRouteVisible,
+}: RouteVisibleButtonProps) => (
+	<TouchableOpacity
+		disabled={!locationVisible}
+		onPress={() => setRouteVisible(!routeVisible)}
+	>
+		<Button disabled={!locationVisible || !routeVisible} name="route" />
+	</TouchableOpacity>
+);
 
 interface ButtonProps {
 	disabled: boolean;
