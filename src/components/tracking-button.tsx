7be58db55224bafa8bf7784951be29b106efd1f7
Riku Rauhala <riku@rauhala.io>
2023-03-20 15:59:40 +0200

    Update linter rules

--- src/components/tracking-button.tsx
+++ src/components/tracking-button.tsx
@@ -6,22 +6,12 @@ import { useTypedSelector } from "../store";
 import Styles from "../styles";
 import theme from "../styles/theme";
 
-const TrackingButton = ({
-	iconName,
-	onPress,
-	text,
-}: TrackingButtonProps): JSX.Element => {
+const TrackingButton = ({ iconName, onPress, text }: TrackingButtonProps): JSX.Element => {
 	const toggled = useTypedSelector((state) => state.ui.trackListVisible);
 	const toggledColor = theme.colors.buttonToggledBackgroundColor;
 
 	return (
-		<View
-			style={
-				toggled
-					? { ...Styles.navigatorButton, backgroundColor: toggledColor }
-					: Styles.navigatorButton
-			}
-		>
+		<View style={toggled ? { ...Styles.navigatorButton, backgroundColor: toggledColor } : Styles.navigatorButton}>
 			<TouchableOpacity onPress={onPress}>
 				<Icon name={iconName} style={Styles.navigatorIcon} />
 				<Text style={Styles.navigatorText}>{text}</Text>
