2b4143b96b1e1550e5da77e6d7f38f732273bb11
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-03-21 13:00:33 +0200

    crypto keys are converted into strings and back to WordArrays
    Co-authored-by: msslotboom <mikael.slotboom@helsinki.fi>

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,20 +1,30 @@
 import CryptoES from "crypto-es";
 import * as Crypto from "expo-crypto";
 
-export const encrypt = (message: string, key: string) => {
-	const encrypted = CryptoES.AES.encrypt(message, key, { iv: generateWordArray(16) }).toString();
+/**
+ * WordArrays are converted to strings. They need to be parsed back into WordArrays for use.
+ *
+ */
+
+export const encrypt = (message: string, keyString: string) => {
+	const keyWordArray = CryptoES.enc.Base64.parse(keyString);
+	const ivString = generateKeyString(16);
+	const ivWordArray = CryptoES.enc.Base64.parse(ivString);
+	const encrypted = CryptoES.AES.encrypt(message, keyWordArray, { iv: ivWordArray }).toString();
 	console.log(encrypted);
 	return encrypted;
 };
 
-export const decrypt = (encryptedMessage: string, key: string) => {
-	const decrypted = CryptoES.AES.decrypt(encryptedMessage, key);
+export const decrypt = (encryptedMessage: string, keyString: string) => {
+	const keyWordArray = CryptoES.enc.Base64.parse(keyString);
+	const decrypted = CryptoES.AES.decrypt(encryptedMessage, keyWordArray);
 	console.log("decrypted: ");
 	console.log(decrypted.toString(CryptoES.enc.Utf8));
 	return decrypted;
 };
 
-export const generateWordArray = (length: number) => {
+export const generateKeyString = (length: number) => {
 	const byteArray = Crypto.getRandomBytes(length);
-	return CryptoES.lib.WordArray.create(byteArray);
+	const keyWordArray = CryptoES.lib.WordArray.create(byteArray);
+	return CryptoES.enc.Base64.stringify(keyWordArray);
 };
