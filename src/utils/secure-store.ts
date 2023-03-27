f3dd2fbded0fe5afb524f829a32f85326dac2de9
Samu Syrjänen <samu.syrjanen@gmail.com>
2023-03-27 13:58:38 +0300

    Secure store handles userId and cryptoKey storage correctly

--- src/utils/secure-store.ts
+++ src/utils/secure-store.ts
@@ -1,134 +1,98 @@
 import * as SecureStore from "expo-secure-store";
 
 /**
- * SecureStore is used to store tracked users' information
+ * SecureStore is used to store tracked users' crypto key
  * in a single JSON object named "tracked". SecureStore also stores the user's
- * crypto key which is used to encrypt and decrypt sent data.
+ * own crypto key. Crypto keys are used to encrypt and decrypt sent data between users.
  *
  * @param {string} userId UserID of the user to be added.
- * @param {string} username Username of the user to be added.
+ * @param {string} cryptoKey The key used to decrypt the shared data.
  * @returns {void}
  *
  * Example JSON object:
  *
  * tracked: {
  *   Y0QsWBzUwP89: {
- *	   id: 1,
- * 	   locationVisible: true,
- * 	   routeVisible: true,
  *	   userId: 'Y0QsWBzUwP89',
- * 	   username: Jorma,
+ *	   cryptoKey: 'sdfdsff',
  *	 },
  *	 W9RZL7VXJ3FK: {
- * 	   id: 2,
- * 	   locationVisible: true,
- *	   routeVisible: false,
  *	   userId: 'W9RZL7VXJ3FK',
- *	   username: Seppo,
+ *	   cryptoKey: '34k0430'
  *	 },
  * }
  *
  * Example cryptokey object:
  *
- * "cryptoKey": "sd9fk3+f0sdf3"
+ * cryptoKey: 'sd9fk3+f0sdf3'
  */
-export const secureStoreAddTracked = async (userId: string, username: string) => {
-	console.log("secureStoreAddTracked()");
+export const secureStoreAddTracked = async (userId: string, cryptoKey: string) => {
 	try {
-		const objectExists = await secureStoreGetTrackedObject();
-		if (!objectExists) {
+		const trackedObject = await SecureStore.getItemAsync("tracked");
+		if (!trackedObject) {
 			await secureStoreInitializeTrackedObject();
-		}
-		const userList = await secureStoreGetAllTrackedUsers();
-		if (userList && userList.includes(userId)) {
-			return;
+		} else if (trackedObject.includes(userId)) {
+			console.log(`${userId} is already added to tracked users. Updating...`);
 		}
 		const tracked = await SecureStore.getItemAsync("tracked");
 		if (tracked) {
 			const trackedJson = JSON.parse(tracked);
-			const trackedObject = {
-				id: 1,
-				locationVisible: true,
-				routeVisible: true,
+			const userObject = {
+				cryptoKey: cryptoKey,
 				userId: userId,
-				username: username,
 			};
 			//store.dispatch(addTrackedUser(trackedObject));
-			trackedJson[userId] = trackedObject;
+			trackedJson[userId] = userObject;
 			const newTracked = JSON.stringify(trackedJson);
 			await SecureStore.setItemAsync("tracked", newTracked);
-			console.log(`Storage updated: ${newTracked}`);
+			console.log(`Secure store: ${userId} added to tracked users.`);
 		}
 	} catch (error) {
-		console.log(`Failed to save key "${userId}". Error: ${error}`);
+		console.log(`Failed to add ${userId} to tracked users. Error: ${error}`);
 	}
 };
 
-export const secureStoreGetTrackedUser = async (key: string) => {
-	console.log("secureStoreGetTrackedUser()");
+export const secureStoreGetTrackedUser = async (userId: string) => {
 	try {
 		const tracked = await SecureStore.getItemAsync("tracked");
 		if (tracked) {
 			const trackedJson = JSON.parse(tracked);
-			const value = trackedJson[key];
+			const value = trackedJson[userId];
 			if (value) {
-				console.log(value);
 				return value;
 			} else {
-				console.log("No values stored under that key.");
+				console.log("No values stored under that userId.");
 			}
 		} else {
 			console.log('"Tracked" object not found.');
 		}
 	} catch (error) {
-		console.log(`Failed to get user "${key}". Error: ${error}`);
+		console.log(`Failed to get user ${userId}. Error: ${error}`);
 	}
 };
 
-export const secureStoreUpdateTrackedUser = async (userId: string, location: boolean, route: boolean) => {
-	console.log("secureStoreUpdateTrackedUser()");
-	try {
-		const tracked = await SecureStore.getItemAsync("tracked");
-		if (!tracked) {
-			console.log("Tracked object not found");
-			return;
-		}
-		const trackedJson = JSON.parse(tracked);
-		// eslint-disable-next-line no-prototype-builtins
-		if (!trackedJson.hasOwnProperty(userId)) {
-			console.log(`User "${userId}" not found.`);
-			return;
-		}
-		trackedJson[userId].locationVisible = location;
-		trackedJson[userId].routeVisible = route;
-		const newTracked = JSON.stringify(trackedJson);
-		await SecureStore.setItemAsync("tracked", newTracked);
-		console.log(`Storage updated: ${newTracked}`);
-	} catch (error) {
-		console.log(`Failed to flip routeVisible for user "${userId}". Error: ${error}`);
-	}
-};
-
-export const secureStoreDeleteTrackedUser = async (key: string) => {
-	console.log("secureStoreDeleteTrackedUser()");
+export const secureStoreDeleteTrackedUser = async (userId: string) => {
 	try {
 		const tracked = await SecureStore.getItemAsync("tracked");
 		if (tracked) {
 			const trackedJson = JSON.parse(tracked);
-			delete trackedJson[key];
+			if (!trackedJson[userId]) {
+				console.log(`Error during deletion: ${userId} not found.`);
+				return;
+			}
+			delete trackedJson[userId];
 			const newTracked = JSON.stringify(trackedJson);
 			await SecureStore.setItemAsync("tracked", newTracked);
-			console.log(`Key "${key}" deleted successfully.`);
+			console.log(`User ${userId} deleted successfully.`);
 		} else {
-			console.log('"tracked" object not found.');
+			console.log('Error: "tracked" object not found.');
 		}
 	} catch (error) {
-		console.log(`Failed to delete key "${key}". Error: ${error}`);
+		console.log(`Failed to delete user ${userId}. Error: ${error}`);
 	}
 };
 
 export const secureStoreInitializeTrackedObject = async () => {
-	console.log("secureStoreInitializeTrackedObject()");
 	try {
 		await SecureStore.setItemAsync("tracked", "{}");
 		console.log('Initialized "tracked" object.');
@@ -137,18 +101,7 @@ export const secureStoreInitializeTrackedObject = async () => {
 	}
 };
 
-export const secureStoreGetAllTrackedUsers = async () => {
-	console.log("secureStoreGetAllTrackedUsers()");
-	try {
-		const tracked = await SecureStore.getItemAsync("tracked");
-		if (tracked) return tracked;
-	} catch (error) {
-		console.log(`Failed to get all users. Error: ${error}`);
-	}
-};
-
 export const secureStoreDeleteTrackedObject = async () => {
-	console.log("secureStoreDeleteTrackedObject()");
 	try {
 		const result = await SecureStore.getItemAsync("tracked");
 		if (result) {
@@ -157,16 +110,6 @@ export const secureStoreDeleteTrackedObject = async () => {
 		console.log('Deleted "tracked" object.');
 	} catch (error) {
 		console.log(`Failed to delete "tracked" object. Error: ${error}`);
-	}
-};
-
-export const secureStoreGetTrackedObject = async () => {
-	console.log("secureStoreGetTrackedObject()");
-	try {
-		const tracked = await SecureStore.getItemAsync("tracked");
-		return tracked;
-	} catch (error) {
-		console.log(`Failed to check whether "tracked" object exists. Error: ${error}`);
 	}
 };
 
