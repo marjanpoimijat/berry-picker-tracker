149b20f5fc476e5c4d0e2fba8c342ae030b93abe
Jyri Kangas <jyri_kangas@outlook.com>
2023-04-03 13:22:57 +0300

    setting s menu

--- src/components/SettingsMenu/SettingsMenu.tsx
+++ src/components/SettingsMenu/SettingsMenu.tsx
@@ -23,18 +23,6 @@ const SettingsMenu = (): JSX.Element => {
 				<View style={{ alignItems: "center" }}>
 					<Text style={SettingsMenuStyles.SettingsMenuTitle}>{languages["Settings"][language]}</Text>
 				</View>
-<<<<<<< HEAD
-=======
-				<View style={SettingsMenuStyles.SettingContainer}>
-					<Text>{languages["Language"][language]}</Text>
-					<LanguageSelect />
-				</View>
-				<MapSelect />
-				<View style={SettingsMenuStyles.SettingContainer}>
-					<Text style={SettingsMenuStyles.SettingsMenuTitle}>{languages["User information"][language]}</Text>
-					<UserInformation />
-				</View>
->>>>>>> 8bc7d01 (Legend for new settings)
 
 				<LanguageSelect />
 
@@ -43,16 +31,8 @@ const SettingsMenu = (): JSX.Element => {
 				<UserInformation />
 
 				<LegalInformation />
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 				<Legend />
-=======
->>>>>>> 0324c15 (settings menu changes!)
-=======
-
-				<Legend />
->>>>>>> 8bc7d01 (Legend for new settings)
 			</ScrollView>
 		</View>
 	);
