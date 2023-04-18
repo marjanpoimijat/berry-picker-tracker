2dfd34fe16a7ce44878bfd54b10a6d3aacf8b7e6
Janne Kärki <janne.karki@helsinki.fi>
2023-04-18 13:52:17 +0300

    Username state to redux fixed

--- src/components/SettingsMenu/UserInformation.tsx
+++ src/components/SettingsMenu/UserInformation.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Text, TextInput, View, TouchableOpacity } from "react-native";
 import { languages } from "../../languages";
 import { identifyUser, resetUser } from "../../reducers/user-reducer";
@@ -6,6 +6,7 @@ import { useTypedDispatch, useTypedSelector } from "../../store";
 import SettingsMenuStyles from "../../styles/SettingsMenuStyles";
 import { createAlert } from "../../utils/alert";
 import theme from "../../styles/theme";
+import { setUsername } from "../../reducers/user-reducer";
 
 /**
  * Renders the settings menu block for changing the user information
@@ -45,6 +46,12 @@ const UserInformation = (): JSX.Element => {
 
 	const [localUsername, setLocalUsername] = useState<string>(username);
 	const userInfo = languages["User information"][language].toUpperCase();
+
+	useEffect(() => {
+		dispatch(setUsername(localUsername));
+		console.log([localUsername]);
+	}, [localUsername]);
+
 	return (
 		<>
 			<View style={SettingsMenuStyles.GreySettingsMenuBlock}>
