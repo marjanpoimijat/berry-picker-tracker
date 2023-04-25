c31a7d6c7dfcfe76f2a4a4e8eba153be865b0ab7
Riku Rauhala <riku@rauhala.io>
2023-04-25 10:07:07 +0300

    Mark the selected map with a blue border

--- src/components/MapMenu/MapMenu.tsx
+++ src/components/MapMenu/MapMenu.tsx
@@ -16,29 +16,36 @@ import { changeMap } from "../../reducers/map-reducer";
 const MapMenu = (): JSX.Element => {
 	const language = useTypedSelector((state) => state.language);
 	const toggled = useTypedSelector((state) => state.ui.mapMenuVisible);
+	const currentMap = useTypedSelector((state) => state.map);
 	const dispatch = useTypedDispatch();
 	const source = "../../../assets/maps";
+
+	console.log(currentMap);
 
 	return (
 		<View style={toggled ? Styles.routeButtonContainer : { ...Styles.routeButtonContainer, display: "none" }}>
 			<MapSelectionButton
 				imageFile={require(`${source}/topo.png`)}
 				onPress={() => dispatch(changeMap(Map.nlsTopographic))}
+				selected={currentMap === Map.nlsTopographic}
 				text={languages["Topographic"][language]}
 			/>
 			<MapSelectionButton
 				imageFile={require(`${source}/osm.png`)}
 				onPress={() => dispatch(changeMap(Map.openStreetMap))}
-				text={"Open Street"}
+				selected={currentMap === Map.openStreetMap}
+				text={"OSM"}
 			/>
 			<MapSelectionButton
 				imageFile={require(`${source}/plain.png`)}
 				onPress={() => dispatch(changeMap(Map.nlsPlain))}
+				selected={currentMap === Map.nlsPlain}
 				text={languages["Plain"][language]}
 			/>
 			<MapSelectionButton
 				imageFile={require(`${source}/satellite.png`)}
 				onPress={() => dispatch(changeMap(Map.nlsAerial))}
+				selected={currentMap === Map.nlsAerial}
 				text={languages["Satellite"][language]}
 			/>
 		</View>
--- src/components/MapMenu/MapSelectionButton.tsx
+++ src/components/MapMenu/MapSelectionButton.tsx
@@ -7,10 +7,11 @@ import Styles from "../../styles";
  *
  * @param {ImageSourcePropType} imageFile The image to be displayed on the button.
  * @param {function} onPress A function to handle the button being pressed.
+ * @param {boolean} selected Selected status of the map button.
  * @param {string} text Text to be displayed on the butotn.
  * @returns {JSX.Element} A new MapSelectionButton component.
  */
-const MapSelectionButton = ({ imageFile, onPress, text }: ButtonProps): JSX.Element => (
+const MapSelectionButton = ({ imageFile, onPress, selected, text }: ButtonProps): JSX.Element => (
 	<View style={Styles.mapSelectionButton}>
 		<TouchableOpacity
 			onPress={onPress}
@@ -18,7 +19,11 @@ const MapSelectionButton = ({ imageFile, onPress, text }: ButtonProps): JSX.Elem
 		>
 			<Image
 				source={imageFile}
-				style={Styles.mapSelectionButtonImage}
+				style={
+					selected
+						? { ...Styles.mapSelectionButtonImage, borderColor: "blue", borderWidth: 1.7 }
+						: Styles.mapSelectionButtonImage
+				}
 			/>
 			<Text style={Styles.buttonText}>{text}</Text>
 		</TouchableOpacity>
