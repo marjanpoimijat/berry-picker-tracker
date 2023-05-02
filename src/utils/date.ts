6b4a2a1e23810895d4e18238f85c13289cb61770
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-05-02 11:40:59 +0300

    Added JSDoc for date util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,10 @@
 import moment from "moment";
-
+/**
+ * Converts time from Unix timestamp to a readable timestamp
+ *
+ * @param {number} dateString - The time as a Unix timestamp in milliseconds
+ * @returns {string} the time in YYYY-MM-DD HH:mm:ss format
+ */
 export const formatDate = (dateString: number): string => {
 	const date = new Date(dateString);
 	const formattedDate = moment.utc(date).format("YYYY-MM-DD HH:mm:ss");
